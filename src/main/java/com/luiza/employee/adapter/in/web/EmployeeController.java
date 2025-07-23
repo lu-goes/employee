47911fb9410e657d53cb41459019a9cdca9e0ba5
Luiza Goes <luizagoes087@gmail.com>
2025-07-23 13:45:56 -0300

    refactor(EmployeeController): complete missing path in postMapping

--- src/main/java/com/luiza/employee/adapter/in/web/EmployeeController.java
+++ src/main/java/com/luiza/employee/adapter/in/web/EmployeeController.java
@@ -3,13 +3,12 @@ package com.luiza.employee.adapter.in.web;
 import com.luiza.employee.adapter.out.persistence.EmployeeJpaEntity;
 import com.luiza.employee.application.usecase.EmployeeService;
 import com.luiza.employee.domain.model.Employee;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,11 @@ public class EmployeeController {
     @GetMapping
     public ResponseEntity<List<Employee>> listAll(){
         return ResponseEntity.ok(employeeService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeJpaEntity> create (@RequestBody @Valid Employee employee){
+        EmployeeJpaEntity saved = employeeService.create(employee);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 }
