2ea0ee7c58b53de3194710dccdf6d02d765df622
Luiza Goes <luizagoes087@gmail.com>
2025-07-23 12:05:23 -0300

    feat(employee + jpa) include domain classes and repository class

--- src/main/java/com/luiza/employee/adapter/out/persistence/EmployeeJpaEntity.java
+++ src/main/java/com/luiza/employee/adapter/out/persistence/EmployeeJpaEntity.java
@@ -1,4 +1,31 @@
 package com.luiza.employee.adapter.out.persistence;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class EmployeeJpaEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String departament;
 }
--- src/main/java/com/luiza/employee/domain/model/Employee.java
+++ src/main/java/com/luiza/employee/domain/model/Employee.java
@@ -1,4 +1,18 @@
 package com.luiza.employee.domain.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
 public class Employee {
+    @NotBlank(message = "Name must not be black")
+    private String name;
+    @Email(message = "Email should be valid")
+    private String email;
+    @NotBlank(message = "Name must not be black")
+    private String departament;
 }
