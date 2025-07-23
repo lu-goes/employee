d2dc5709e13e0e78088247637a4aaca77379905d
Luiza Goes <luizagoes087@gmail.com>
2025-07-23 12:52:32 -0300

    feat(employeeservice): add method to list employees

--- src/main/java/com/luiza/employee/application/usecase/EmployeeService.java
+++ src/main/java/com/luiza/employee/application/usecase/EmployeeService.java
@@ -1,9 +1,20 @@
 package com.luiza.employee.application.usecase;
 
+import com.luiza.employee.domain.model.Employee;
+import com.luiza.employee.domain.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class EmployeeService {
 
+    private EmployeeRepository repository;
+
+    public List<Employee> getAll(){
+        return repository.findAll();
+    }
 
 }
